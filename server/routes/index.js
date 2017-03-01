4842dd78b2738c4f70ee664e154ad629e8d187a2
andela-pbirir <purity.birir@andela.com>
2017-03-01 15:28:57 +0300

    [feature #139284015] Add the various routes required

---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+const documentsController = require('../controllers').documents;
+const rolesController = require('../controllers').roles;
+
+module.exports = (app) => {
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the documents API!',
+  }));
+  app.post('/api/documents', documentsController.create);
+  app.post('/api/documents/:rolesId/role', rolesController.create);
+  app.get('/api/documents', documentsController.list);
+  app.post('/api/documents', rolesController.create);
+};
+
