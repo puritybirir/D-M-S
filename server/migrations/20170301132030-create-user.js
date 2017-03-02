4ae5e5543d4380acddebd23049cf040f8437b0b8
andela-pbirir <purity.birir@andela.com>
2017-03-02 20:59:40 +0300

    [feature #39284015] Add migrations

---
+++ server/migrations/20170301132030-create-user.js
@@ -0,0 +1,48 @@
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      roleId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Roles',
+          key: 'id',
+          as: 'roleId',
+        },
+      },
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('users');
+  },
+};
---
+++ server/migrations/20170302074719-create-document.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('documents', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      content: {
+        type: Sequelize.STRING,
+      },
+      access: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id',
+          as: 'userId',
+        },
+      },
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('documents');
+  },
+};
