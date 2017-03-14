0a16b7cb9cc5819445d336e08e769a06e0dbbb61
andela-pbirir <purity.birir@andela.com>
2017-03-14 12:18:11 +0300

    [feature #140960977] Add endpoints to find one document and update it

--- server/controllers/documents.js
+++ server/controllers/documents.js
@@ -10,13 +10,71 @@ module.exports = {
       .then(document => res.status(201).send(document))
       .catch(error => res.status(400).send(error));
   },
-  list(req, res) {
+  listAll(req, res) {
     return documents
     .all()
     .then(document => res.status(200).send(document))
     .catch(error => res.status(400).send(error));
   },
+
+  findOne(req, res) {
+    return documents
+    .findById(req.params.id)
+    .then((document) => {
+      if (!document) {
+        return res.status(404).send({
+          message: 'Document Not Found',
+        });
+      }
+      return res.status(200).send(document);
+    })
+    .catch(error => res.status(400).send(error));
+  },
+
+  update(req, res) {
+    return documents
+    .findById(req.params.id)
+    .then((document) => {
+      if (!document) {
+        return res.status(404).send({
+          message: 'Document Not Found',
+        });
+      }
+      // Filter
+      const updatedDocument = {
+        title: req.body.title,
+        content: req.body.content,
+      };
+
+      return document
+        .update(updatedDocument, { fields: Object.keys(updatedDocument) })
+        .then(doc => res.status(200).send(doc))
+        .catch(error => res.status(400).send(error));
+    })
+    .catch(error => res.status(400).send(error));
+  },
+
+  delete(req, res) {
+    return documents
+    .find({
+      where: {
+        id: req.params.documentsId,
+        userId: req.params.userId,
+      },
+    })
+    .then((document) => {
+      if (!document) {
+        return res.status(404).send({
+          message: 'Documetn Not Found',
+        });
+      }
+
+      return document
+        .destroy()
+        .then(() => res.status(204).send())
+        .catch(error => res.status(400).send(error));
+    })
+    .catch(error => res.status(400).send(error));
+  },
 };
-
-
 
