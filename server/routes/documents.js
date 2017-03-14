8225edebccc097e4b16edf94568d34e493addc74
andela-pbirir <purity.birir@andela.com>
2017-03-14 15:30:46 +0300

    [feature #140960977] Add route to delete a document and authenticate the routes

--- server/routes/documents.js
+++ server/routes/documents.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const authenticate = require('../helpers/middlewares').authenticate;
 
 const router = express.Router();
 const documentsController = require('../controllers').documents;
+
+// middleware authenitcated
+router.use(authenticate);
 
 router.post('/', documentsController.create);
 router.get('/', documentsController.listAll);
 router.get('/:id', documentsController.findOne);
 router.put('/:id', documentsController.update);
+router.delete('/:id', documentsController.delete);
 
 module.exports = router;
