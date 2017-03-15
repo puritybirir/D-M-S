b91eabdb0e17c86ddcd473d85ce996ce76a31951
andela-pbirir <purity.birir@andela.com>
2017-03-15 22:47:31 +0300

    [feature #140960977] Change routes to fit classes

--- server/routes/documents.js
+++ server/routes/documents.js
@@ -4,13 +4,14 @@ const authenticate = require('../helpers/middlewares').authenticate;
 const router = express.Router();
 const documentsController = require('../controllers').documents;
 
+const doc = new documentsController();
 // middleware authenitcated
 router.use(authenticate);
 
-router.post('/', documentsController.create);
-router.get('/', documentsController.listAll);
-router.get('/:id', documentsController.findOne);
-router.put('/:id', documentsController.update);
-router.delete('/:id', documentsController.delete);
+router.post('/', doc.create);
+router.get('/', doc.listAll);
+router.get('/:id', doc.findOne);
+router.put('/:id', doc.update);
+router.delete('/:id', doc.delete);
 
 module.exports = router;
