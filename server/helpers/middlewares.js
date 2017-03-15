d2346ae74040da0aabbb5af22ab306ee072b896a
andela-pbirir <purity.birir@andela.com>
2017-03-15 22:38:42 +0300

    [feature #140960977] Add method for decoding the token

--- server/helpers/middlewares.js
+++ server/helpers/middlewares.js
@@ -2,19 +2,20 @@ const jwt = require('jsonwebtoken');
 
 const authenticate = (req, res, next) => {
   const token = req.query.token || req.headers['x-access-token'];
-  if (!token) {
+  if (!token)
     return res.status(401).send({
       success: false,
       message: 'No token provided',
     });
-  }
 
-  return jwt.verify(token, process.env.SECRET, (err) => {
+  return jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).json({
         success: false,
         messsage: 'Invalid token provided',
       });
+    } else {
+      req.tokenDecode = decoded;
     }
     return next();
   });
