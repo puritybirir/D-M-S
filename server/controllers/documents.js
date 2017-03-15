1343f77ee41a4dbf6dc9d580be31818e6ba432c1
andela-pbirir <purity.birir@andela.com>
2017-03-15 22:37:02 +0300

    [feature #140960977] Change the document controller to include classes

--- server/controllers/documents.js
+++ server/controllers/documents.js
@@ -1,22 +1,30 @@
 const documents = require('../models').document;
 
-module.exports = {
+/** Class representing a Document. */
+class Document {
+  /**
+ * Creates new user
+ * @param {object} req
+ * @param {object} res
+ */
   create(req, res) {
     return documents
+      // .create(req.body)
       .create({
         title: req.body.title,
         content: req.body.content,
+        access: req.body.access || 'private',
+        userId: req.tokenDecode.userId
       })
       .then(document => res.status(201).send(document))
       .catch(error => res.status(400).send(error));
-  },
+  }
   listAll(req, res) {
     return documents
     .all()
     .then(document => res.status(200).send(document))
     .catch(error => res.status(400).send(error));
-  },
-
+  }
   findOne(req, res) {
     return documents
     .findById(req.params.id)
@@ -29,7 +37,7 @@ module.exports = {
       return res.status(200).send(document);
     })
     .catch(error => res.status(400).send(error));
-  },
+  }
 
   update(req, res) {
     return documents
@@ -52,7 +60,7 @@ module.exports = {
         .catch(error => res.status(400).send(error));
     })
     .catch(error => res.status(400).send(error));
-  },
+  }
 
   delete(req, res) {
     return documents
@@ -64,15 +72,19 @@ module.exports = {
     .then((document) => {
       if (!document) {
         return res.status(404).send({
-          message: 'Documetn Not Found',
+          message: 'Document Not Found',
         });
       }
       return document
         .destroy()
-        .then(() => res.status(204).send())
+        .then(() => res.status(204).send({
+          message: 'Document deleted succesfuly',
+        })
+        )
         .catch(error => res.status(400).send(error));
     })
     .catch(error => res.status(400).send(error));
-  },
-};
+  }
+}
 
+exports.Document = Document;
