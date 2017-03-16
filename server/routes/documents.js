c43298b9b44ad8c923d9ea237cb8cfbe0c2f4802
andela-pbirir <purity.birir@andela.com>
2017-03-16 15:54:08 +0300

    [feature #140960977] Refactor routes to reflect the checkpoint finale doc

--- server/routes/documents.js
+++ server/routes/documents.js
@@ -8,10 +8,11 @@ const doc = new documentsController();
 // middleware authenitcated
 router.use(authenticate);
 
-router.post('/', doc.create);
-router.get('/', doc.listAll);
-router.get('/:id', doc.findOne);
-router.put('/:id', doc.update);
-router.delete('/:id', doc.delete);
+router.post('/documents', doc.create);
+router.get('/documents', doc.listAll);
+router.get('/documents/:id', doc.findOne);
+router.put('/documents/:id', doc.update);
+router.delete('/documents/:id', doc.delete);
+router.get('/search/documents', doc.search);
 
 module.exports = router;
