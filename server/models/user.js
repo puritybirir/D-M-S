b976928d9ff1b9ba53fcc81264edd7052b0fe328
andela-pbirir <purity.birir@andela.com>
2017-03-16 15:53:26 +0300

    [feature #140960977] Add userName to the user model

--- server/models/user.js
+++ server/models/user.js
@@ -10,6 +10,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
