19653f0bdba857501420582f1cacc8d901f21d1a
andela-pbirir <purity.birir@andela.com>
2017-03-27 14:30:27 +0300

    [feature #141878503] create dummy data for the roles seeder

--- server/seeders/20170324125925-RolesSeeder.js
+++ server/seeders/20170324125925-RolesSeeder.js
@@ -1,7 +1,32 @@
-module.exports = {
-  up(queryInterface, Sequelize) {
-  },
+const faker = require('faker');
 
-  down(queryInterface, Sequelize) {
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Roles', [
+    {
+      name: 'user',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'trialUser',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'coolUser',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+
+  ], {}),
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Roles', null, {});
   }
 };
+
+
