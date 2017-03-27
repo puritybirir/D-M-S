6e146f3a336a687e3d833e9ff28df7131b1dde89
andela-pbirir <purity.birir@andela.com>
2017-03-27 14:29:53 +0300

    [feature #141878503] put the proper crud methods for the role methods

--- server/routes/roles.js
+++ server/routes/roles.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const authenticate = require('../helpers/middlewares').authenticate;
 
 const router = express.Router();
 const rolesController = require('../controllers').roles;
 
 const roles = new rolesController();
+router.use(authenticate);
 
 router.post('/roles', roles.create);
-router.post('/roles/:id', roles.findOne);
-router.get('/roles/:id', roles.update);
+router.get('/roles/:id', roles.findOne);
+router.put('/roles/:id', roles.update);
 router.get('/roles', roles.listAll);
 router.delete('/roles/:id', roles.delete);
 
