cde612fa921acb41abd1c3790fefc5d87d402cb4
andela-pbirir <purity.birir@andela.com>
2017-03-30 11:32:20 +0300

    [feature #142608843] require documents

--- server/helpers/middlewares.js
+++ server/helpers/middlewares.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const documents = require('../models').document;
 
 const authenticate = (req, res, next) => {
   const token = req.query.token || req.headers['x-access-token'];
@@ -35,6 +36,45 @@ const confirmAdmin = (req, res, next) => {
   return next();
 };
 
+const docUserAccess = (req, res, next) => {
+  if (req.tokenDecoded.roleId === '1') {
+    return documents
+    .find({
+      where: {
+        userId: req.tokenDecoded.userId
+      },
+    })
+    .then((docs) => {
+      req.data = docs;
+      next();
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+  }
+  next();
+};
 
-module.exports = { authenticate, confirmAdmin };
+const docAdminAccess = (req, res, next) => {
+  if (req.tokenDecoded.roleId === '2') {
+    return documents
+    .find({
+      where: {
+        access: 'private' && 'public'
+      },
+    })
+    .then((docs) => {
+      req.data = docs;
+      next();
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+  }
+  next();
+};
+
+
+
+module.exports = { authenticate, confirmAdmin, docAdminAccess, docUserAccess };
 
