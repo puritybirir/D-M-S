701b1f44248a6e1622b18e1d290d724d7c468d32
andela-pbirir <purity.birir@andela.com>
2017-04-06 18:17:36 +0300

    [feature 140583907] set up hello world

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
-      <div style={{ textAlign: 'center' }}>
-        <h1>Hello World</h1>
-      </div>);
+      <h1>Hello from React</h1>
+    );
   }
 }
+
+export default App;
+
