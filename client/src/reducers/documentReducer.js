2384bc0d9610af5c2d55f064fc82094d53eba909
andela-pbirir <purity.birir@andela.com>
2017-04-09 16:32:45 +0300

    [feature 143305379] Fix state bug

--- client/src/reducers/documentReducer.js
+++ client/src/reducers/documentReducer.js
@@ -3,7 +3,6 @@ import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
 export default function documentReducer(state = initialState.documents, action) {
-  console.log(action.document, 'ewjfqon');
   switch (action.type) {
     case types.LOAD_DOCUMENTS_SUCCESS:
       return action.documents;
