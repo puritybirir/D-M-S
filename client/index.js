61795dca01c3928e44c9f3287098e5a5cda78221
andela-pbirir <purity.birir@andela.com>
2017-04-09 08:39:57 +0300

    [feature 143305379] delete unnecessary files

--- client/index.js
+++
@@ -1,8 +0,0 @@
-/*
-    ./client/index.js
-*/
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
-
-render(<App />, document.getElementById('root'));
