68971808945ed879ea85f4c792696b8273a09f05
andela-pbirir <purity.birir@andela.com>
2017-04-09 16:28:29 +0300

    [feature 143305379] Ensure that token is bein picked from the backend

--- client/src/api/documentApi.js
+++ client/src/api/documentApi.js
@@ -1,20 +1,18 @@
 import request from 'superagent';
 
-const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaGVuZGVvZUBleGFtcGxlLmNvbSIsInVzZXJJZCI6MTQsInJvbGVJZCI6IjIiLCJpYXQiOjE0OTE3MTM2NjUsImV4cCI6MTQ5MTcxNzI2NX0.bYRbfZXnX4pl3j2z-KsYm7Vf2bBzWI7Q_TCWMNekm0U';
 class DocumentApi {
   static getAllDocuments() {
     return request
     .get('/api/documents')
-    .set('x-access-token', userToken)
+    .set('x-access-token', window.localStorage.getItem('token'))
     .then(res => res.body.document)
     .catch(error => error);
   }
 
   static createDocument(docDetails) {
-    console.log(docDetails, 'DETAILS');
     return request
     .post('/api/documents')
-    .set('x-access-token', userToken)
+    .set('x-access-token', window.localStorage.getItem('token'))
     .send(docDetails)
     .then(res => res.body.document)
     .catch(error => error);
