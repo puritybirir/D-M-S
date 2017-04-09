3d6c716c3259af669efa8522499f4cdaefe1ec52
andela-pbirir <purity.birir@andela.com>
2017-04-09 08:41:36 +0300

    [feature 143305379] configure the various routes for the application

---
+++ client/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './components/App';
+import HomePage from './components/home/HomePage';
+import DocumentsPage from './components/Documents/DocumentsPage';
+import DocumentPage from './components/Documents/DocumentPage';
+import NewDocumentPage from './components/Documents/NewDocumentPage';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage} />
+    <Route path="/documents" component={DocumentsPage} >
+      <Route path="/documents/new" component={NewDocumentPage} />
+      <Route path="/documents/:id" component={DocumentPage} />
+    </Route>
+  </Route>
+);
