8f31a13d782be8be4485fe3513efcf874e1fee8c
andela-pbirir <purity.birir@andela.com>
2017-04-09 16:35:56 +0300

    [feature #143305379] configure the routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -5,13 +5,15 @@ import HomePage from './components/home/HomePage';
 import DocumentsPage from './components/Documents/DocumentsPage';
 import DocumentPage from './components/Documents/DocumentPage';
 import NewDocumentPage from './components/Documents/NewDocumentPage';
+import SignUpPage from './components/Auth/SignUp/SignUpPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
+    <Route path="/signup" component={SignUpPage} />
     <Route path="/documents" component={DocumentsPage} >
-      <Route path="/documents/new" component={NewDocumentPage} />
-      <Route path="/documents/:id" component={DocumentPage} />
+    <Route path="/documents/new" component={NewDocumentPage} />
+    <Route path="/documents/:id" component={DocumentPage} />
     </Route>
   </Route>
 );
